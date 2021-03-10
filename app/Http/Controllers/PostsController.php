098c4d9e1cd506a4966f0f79ef8bbfd81b64e12a
YuqiWang <ohikuki@gmail.com>
2021-03-10 09:00:29 +0000

    Laravel基礎課題5

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,4 +12,22 @@ class PostsController extends Controller
         $posts = Post::orderBy('created_at', 'desc')->get();
         return view('posts.index', ['posts' => $posts]);
     }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Request $request)
+    {
+        // dd($request);
+        $params = $request->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:140',
+            ]);
+        
+        Post::create($params);
+        
+        return redirect()->route('top');
+    }
 }
