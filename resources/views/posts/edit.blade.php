62a8de43c38ca232888764b7a2afc2b451a91da0
YuqiWang <ohikuki@gmail.com>
2021-03-11 13:26:12 +0000

    Laravel基礎課題6完了

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,12 +3,11 @@
     <div class="container mt-4">
      <div class="border p-4">
         <h1 class="h5 mb-4">
-           投稿の新規作成
+           投稿の編集
         </h1>
-        <form method="POST" action="{{ route('posts.store')}}">
+        <form method="POST" action="{{ route('posts.update', ['post' =>$post])}}">
            {{csrf_field()}}
            {{ method_field('PUT')}}
-           
            <fieldset class="mb-4">
                <div class="form-group">
                   <label for="title">
@@ -18,6 +17,7 @@
                       id="title"
                       name="title"
                       class="form-control"
+                      value="{{$post->title}}"
                       type="text" 
                    >
              </div>
@@ -30,15 +30,14 @@
                       name="body"
                       class="form-control"
                       rows="4"
-                  >
-                  </textarea>
+                  >{{$post->body}}</textarea>
              </div>
              <div class="mt-5">
-                <a class="btn btn-secondary" href="{{route('top')}}">
+                <a class="btn btn-secondary" href="{{ route('posts.show', ['post' =>$post])}}">
                    キャンセル
                 </a>
                 <button type="submit" class="btn btn-primary">
-                 投稿する
+                 更新する
                 </button>
              </div>
            </fieldset>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,13 +3,7 @@
 @section('content')
 
     <div class="container mt-4">
-        <div class="mb-4">
-            <a href="{{route('posts.create')}}" class="btn btn-primary">
-                投稿を新規作成する
-            </a>
-        </div>
         
-        @foreach ($posts as $post)
         <div class="card mb-4">
             <div class="card-header mb-2">
                 {{ $post->title }}
@@ -18,9 +12,6 @@
                 <p class="card-text">
                 {{ $post->body }}
                 </p>
-                <a class="card-link" href={{ route('posts.show', ['post' => $post])}}>
-                    詳細を見る
-                </a>
             </div>
             <div class="card-footer">
                 <span class="mr-2">
@@ -28,8 +19,21 @@
                 </span>
             </div>
         </div>
-        @endforeach
+        
+        <div class="mt-4 text-right">
+            <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post])}}">
+               編集
+            </a>
+            <form
+            style="display: inline-block;"
+            method="POST"
+            action="{{route('posts.destroy', ['post' => $post])}}">
+                {{csrf_field()}}
+                {{ method_field('DELETE')}}
+                <button class="btn btn-danger">削除</button>
+            </form>
+        </div>
     </div>
-
-
+    
+    
 @endsection('content')
